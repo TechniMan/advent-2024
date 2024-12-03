c39402a49cd78814f373ac2add1f2e729b8a3bff
Will Thomas <technimanx@gmail.com>
2024-12-03 01:26:08 +0000

    Completed 2-2

--- day2.py
+++ day2.py
@@ -1,15 +1,8 @@
-reports = []
-
-with open("day2.txt") as file:
-    reports = file.readlines()
-
-total_safe = 0
-for report in reports:
-    levels = list(map(int, report.removesuffix("\n").split(" ")))
-    safe = False
+def is_report_safe(levels: list[int], tolerance: int = 1) -> bool:
+    safe: bool = False
 
     # all increasing
-    previous = levels[0]
+    previous: int = levels[0]
     for l in range(1, levels.__len__()):
         if levels[l] <= previous:
             safe = False
@@ -17,8 +10,8 @@ for report in reports:
         safe = True
         previous = levels[l]
 
-    # OR all decreasing
-    if safe == False:
+    # OR all decreasing; only check if above failed
+    if not safe:
         previous = levels[0]
         for l in range(1, levels.__len__()):
             if levels[l] >= previous:
@@ -27,8 +20,8 @@ for report in reports:
             safe = True
             previous = levels[l]
 
-    # safe so far AND adjacent levels differ by at least one and at most three
-    if safe == True:
+    # either of above passed AND adjacent levels differ by at least one and at most three
+    if safe:
         previous = levels[0]
         for l in range(1, levels.__len__()):
             diff = abs(levels[l] - previous)
@@ -38,7 +31,42 @@ for report in reports:
             safe = True
             previous = levels[l]
 
-    if safe == True:
-        total_safe += 1
+    return safe
 
-print(total_safe)
+
+
+reports = []
+
+with open("day2.txt") as file:
+    reports = file.readlines()
+
+total_safe = 0
+for report in reports:
+    levels = list(map(int, report.removesuffix("\n").split(" ")))
+
+    # check entire report is safe
+    safe = is_report_safe(levels)
+
+    # if unsafe, check iterations of the report to determine if it can be dampened
+    if not safe:
+        # unsafe until proven safe
+        safe = False
+        # retest, removing each level, to see if problem can be dampened
+        # ign is level idx to ignore
+        for ign in range(levels.__len__()):
+            copy = levels.copy()
+            copy.pop(ign)
+            safe = is_report_safe(copy, 0)
+            # if we had a completely safe run:
+            if safe:
+                break
+
+    total_safe += safe
+    if safe:
+        #print("+ Safe: ", report.removesuffix("\n"))
+        pass
+    else:
+        #print("- Unsafe: ", report.removesuffix("\n"))
+        pass
+
+print("Total: ", total_safe)
